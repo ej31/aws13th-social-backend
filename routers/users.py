5ac030eff5893781bbf7dd48995505dce68a79eb
Moriental <youngwoo7804@gmail.com>
2026-01-14 16:54:25 +0900

    feat implement user signup API with form-data and profile image upload

---
+++ routers/users.py
@@ -0,0 +1,41 @@
+import os
+from typing import Annotated
+from fastapi import APIRouter, status, UploadFile
+from fastapi.params import Form, File, Depends
+from pydantic import EmailStr
+
+from common.config import settings
+from common.dependencies import get_auth_service
+from schemas.user import SignupRequest, UserResponse
+from common.common import CommonResponse
+from services.user_service import UserService
+router= APIRouter(prefix="/users",tags=["users"])
+
+#현재 json 주소 (나중에 DB로 마이그레이션 예정)
+DB_PATH = settings.database_path
+
+#올라갈 이미지 저장소 (현재 폴더 경로 내의 static/profiles)
+UPLOAD_PATH = settings.upload_dir
+
+#만약 해당 폴더가 없을 시 폴더 생성
+os.makedirs(UPLOAD_PATH, exist_ok=True)
+
+@router.post(path= "/",response_model= CommonResponse[UserResponse],status_code=status.HTTP_201_CREATED)
+async def signup(
+    user_service: Annotated[UserService, Depends(get_auth_service)],
+    email: EmailStr = Form(...), #...은 requirement(필수), EmailStr (@ 형식으로 들어온 데이터)
+    password: str = Form(...),
+    nickname: str = Form(...),
+    profileImage: UploadFile = File(None),
+):
+    signup_data = SignupRequest(email=email,password=password,nickname=nickname)
+    new_user = await user_service.signup_user(signup_data,profileImage)
+
+    return CommonResponse(
+        messages="회원가입에 성공했습니다.",
+        data=new_user
+    )
+#
+# @router.post("/login",response_model= CommonResponse[TokenData],status_code=status.HTTP_200_OK)
+# async def login(login_data: UserLogin):
+#     return True

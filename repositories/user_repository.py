1f488a67e2e9274ca7bcd418d8e6d972e749d4b6
Moriental <youngwoo7804@gmail.com>
2026-01-15 15:11:49 +0900

    developing a user deletion function in user_repositoris...

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -20,20 +20,19 @@ class UserRepository:
                 return []
 
     #내부용 함수
-    def _save_all(self, users: list[dict]):
+    def _save_all(self, users: list[dict]) -> None:
         with open(self.file_path, "w", encoding="utf-8") as f:
             json.dump(users, f, indent=4, ensure_ascii=False)
 
     def find_by_email(self,email:str) -> Optional[dict]:
         users = self._load_all()
-
         for user in users:
             if user["email"] == email:
                 return user
 
         return None
 
-    def find_by_id(self,id:str) -> Optional[dict]:
+    def find_by_id(self,id:int) -> Optional[dict]:
         users = self._load_all()
         for user in users:
             if user["id"] == id:
@@ -59,6 +58,20 @@ class UserRepository:
 
     def get_next_id(self) -> int:
         users = self._load_all()
+
+        #유저가 존재하지 않으면 id는 1이 된다.
         if not users:
             return 1
+        #id값이 제일 높은 값을 찾은 뒤 1을 더한 값을 id로 함
         return max(user["id"] for user in users) + 1
+
+    def delete_by_email(self,email:str):
+        #유저 전체를 메모리로 가져온다.
+        users = self._load_all()
+
+        #삭제할 이메일이 아닌 유저들만 골라낸다.
+        filter_user = [user for user in users if user["email"]!=email]
+
+        #걸러낸 목록을 파일에 저장한다.
+        self._save_all(filter_user)
+

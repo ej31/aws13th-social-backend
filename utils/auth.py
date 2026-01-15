b20e4b682cf42c9aca51f34503497ba8016add84
jmw010314 <jmw010314@naver.com>
2026-01-15 11:34:06 +0900

    jwt 토큰 추가

--- utils/auth.py
+++ utils/auth.py
@@ -1,8 +1,19 @@
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
-
+import os
+from datetime import datetime,timedelta
+from jose import jwt
+from dotenv import load_dotenv
 # 해시 객체 생성
 ph = PasswordHasher()
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
+)
 
 def get_password_hash(password: str) -> str:
 
@@ -14,3 +25,9 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
         return ph.verify(hashed_password, plain_password)
     except VerifyMismatchError:
         return False
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)

bda8e77c422a3fc2ba849285e84b90a9aa52da27
Moriental <youngwoo7804@gmail.com>
2026-01-16 10:13:32 +0900

    refactor: modularize JWT authentication logic

--- common/security.py
+++ common/security.py
@@ -1,7 +1,5 @@
 from hashids import Hashids
-from jose import jwt, JWTError
 from passlib.context import CryptContext
-from datetime import datetime, timedelta, timezone
 
 from common.config import settings
 
@@ -27,31 +25,4 @@ def decode_id(id: str) -> int:
     # 해시를 하게 되면 튜플 형식으로 저장됨 (6,)... 그렇기에 [0]번째 인수만 반환하게 함
     return decoded[0] if decoded else None
 
-#access Token 생성
-def create_access_token(subject:str) -> str:
-    minutes = int(settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    expire = datetime.now(timezone.utc) + timedelta(minutes=minutes)
-    to_encode = {"exp": expire,
-                 "sub":str(subject)}
-
-    encoded_jwt = jwt.encode(
-        to_encode,
-        settings.SECRET_KEY,
-        algorithm=settings.ALGORITHM
-    )
-    return encoded_jwt
-
-def decode_access_token(token: str) -> str | None:
-    try:
-        payload = jwt.decode(token,
-                             settings.SECRET_KEY,
-                             algorithms=[settings.ALGORITHM]
-                             )
-        email: str= payload.get("sub")
-        if not email:
-            return None
-        return email
-    except JWTError:
-        #토큰이 만료되거나 없을 경우
-        return None
 

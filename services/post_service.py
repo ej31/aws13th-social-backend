187beb8823456a508d5a503ce956e27fa5f338a4
kim <youngwoo7804@gmail.com>
2026-01-17 21:10:43 +0900

    fix: resolve ResponseValidationError in post list by assembling author data

--- services/post_service.py
+++ services/post_service.py
@@ -4,6 +4,7 @@ from typing import Annotated
 from fastapi import HTTPException,status
 from fastapi.params import Depends
 
+from common.security import decode_id, encode_id
 from repositories.post_repository import PostRepository
 from repositories.user_repository import UserRepository
 from schemas.post import PostsCreateRequest, PostsUpdateRequest
@@ -12,6 +13,7 @@ from schemas.post import PostsCreateRequest, PostsUpdateRequest
 class PostService:
     def __init__(self,post_repo:Annotated[PostRepository, Depends(PostRepository)],
                       user_repo:Annotated[UserRepository,Depends()]) -> None:
+        self.user_repo = user_repo
         self.post_repo = post_repo
 
     @staticmethod
@@ -23,15 +25,25 @@ class PostService:
     async def create_post(self,req:PostsCreateRequest, author_id : str) -> dict:
         post_data = req.model_dump()
 
-        post_data["author_id"] = author_id
         post_data["created_at"] = datetime.now(timezone.utc).isoformat()
         post_data["views"] = 0
         post_data["likes"] = 0
         post_data["comment_count"]=0
+        post_data["author_id"] = author_id
         saved_post = self.post_repo.save(post_data)
 
         author_info = self.user_repo.find_by_id(author_id)
-        return self.post_repo.save(post_data)
+
+        print(f"DEBUG: author_info: {author_info}")
+        print(f"DEBUG: author_id(raw)={author_id}") # 가장 확실한 확인 방법
+
+        saved_post["author"] ={
+            "id": encode_id(author_info.get("id")),
+            "nickname": author_info.get("nickname")
+        }
+
+        saved_post["is_liked"] = False
+        return saved_post
 
     async def get_posts(self,page:int =1,limit:int = 10):
         all_posts = self.post_repo.find_all()
@@ -43,7 +55,22 @@ class PostService:
         #즉 1페이지 보여줘 하면 0번 인덱스부터 보여줘야 하므로
         end = start + limit
         #end는 끝까지 보여줄 페이지 즉 0+10 =10 10페이지가 마지막
-        return all_posts[start:end], len(all_posts)
+
+        paged_post = all_posts[start:end]
+        #페이지네이션으로 가공된 리스트
+        for post in paged_post:
+            author_id = post.get("author_id")
+            author_info = self.user_repo.find_by_id(author_id)
+
+            if author_info:
+                post["author"] = {
+                    "id": encode_id(author_info.get("id")),
+                    "nickname": author_info.get("nickname"),
+                }
+            #좋아요의 기본값은 false
+            post["is_liked"] = False
+
+        return paged_post, len(all_posts)
 
     async def get_posts_detail(self,post_id:int,user_id:str | None = None) -> dict:
         posts_data = self.post_repo.find_by_id(post_id)

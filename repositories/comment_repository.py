2eebf7ef2d5d7c59b9aebb80e8371f12287b79d1
kim <youngwoo7804@gmail.com>
2026-01-17 23:14:24 +0900

    feat: implement CommentRepository and fix deletion logic

---
+++ repositories/comment_repository.py
@@ -0,0 +1,60 @@
+import os
+import json
+from typing import Optional
+
+from common.config import settings
+
+class CommentRepository:
+    def __init__(self) -> None:
+        self.file_path = settings.comments_json_path
+        if not os.path.exists(self.file_path):
+            self._save_all([])
+    def _load_all(self) -> list[dict]:
+        with open(self.file_path, "r", encoding="utf-8") as f:
+            try:
+                return json.load(f)
+            except json.JSONDecodeError:
+                return []
+
+    def _save_all(self,posts) -> None:
+        with open(self.file_path, "w", encoding="utf-8") as f:
+            json.dump(posts, f, indent=4, ensure_ascii=False)
+
+    #특정 게시글에 속한 모든 댓글 가져오기
+    def find_all_by_post_id(self,post_id: int) -> list[dict]:
+        comments = self._load_all()
+        return [c for c in comments if int(c["post_id"]) == int(post_id)]
+
+    def find_by_id(self, comment_id: int) -> Optional[dict]:
+        comments = self._load_all()
+        for comment in comments:
+            if int(comment["comment_id"]) == int(comment_id):
+                return comment
+        return None
+
+    def save(self,comment_data:dict) -> dict:
+        comments = self._load_all()
+
+        if "comment_id" not in comment_data or comment_data["comment_id"] is None:
+            max_id = max((c["comment_id"] for c in comments), default=0)
+            comment_data["comment_id"] = max_id + 1
+            comments.append(comment_data)
+
+        else:
+            for i,c in enumerate(comments):
+                if int(c["comment_id"]) == int(comment_data["comment_id"]):
+                    comments[i] = comment_data
+                    break
+            else:
+                raise ValueError(f"댓글 ID {comment_data['comment_id']}를 찾을 수 없습니다.")
+        self._save_all(comments)
+        return comment_data
+
+    def delete(self,comment_id: int) -> bool:
+        comments = self._load_all()
+        filtered_data = [c for c in comments if c["comment_id"] == comment_id]
+
+        if len(comments) == len(filtered_data):
+            return False
+
+        return True

e84710a541b425033249f32b13abf5c5ac12e77b
trieeun <ri2eeuntt@gmail.com>
2026-01-20 10:34:49 +0900

    fix: allow_origins 수정

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app = FastAPI(
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

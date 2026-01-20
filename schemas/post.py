4c02a99c9826bddbdc004b48b7ad2c47264f01c2
Moriental <youngwoo7804@gmail.com>
2026-01-20 11:21:39 +0900

    refactor(schemas): implement Internal models and update ID types to int

--- schemas/post.py
+++ schemas/post.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime,timezone
 
 from pydantic import BaseModel, Field
 
@@ -22,7 +22,6 @@ class PostsAuthorResponse(BaseModel):
     id: str = Field(...,description="해시된 사용자 ID")
     nickname:str
 
-#게시글 상세 조회
 #게시물 목록을 조회할때는 PaginatedResponse(Generic[T], BaseModel) 함수를 이용함
 class PostsResponse(PostsBase):
     post_id: int = Field(...,description="게시물의 아이디")
@@ -33,6 +32,18 @@ class PostsResponse(PostsBase):
     views: int = Field(0,description="게시물의 조회수 개수")
     created_at: datetime
     updated_at: datetime | None = None
+
+#JSON 내부에 저장할 때 사용하는 모델
+class PostsInternal(PostsBase):
+    post_id: int
+    author_id: int # 내부에서는 해시되지 않은 원본 ID를 저장한다.
+    views: int = 0
+    likes: int = 0
+    comment_count: int = 0
+    #default_factory는 새로운 객체가 만들어질 때 함수를 실행해서 새로운 값을 만들어냄
+    #datetime.now().isoformat()으로 생성하게 되면 서버가 최초 실행된 날을 기준으로 날짜가 정해짐
+    created_at: str = Field(default_factory=lambda:datetime.now(timezone.utc).isoformat())
+    updated_at: str | None = None
 
 class PostsResponseDetail(PostsResponse):
     pass

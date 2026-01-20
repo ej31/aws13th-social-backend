0fa75aa14327cbd0e800ea1b68b07f096c345934
eewwqq1 <rlaehdrbs90@gmail.com>
2026-01-20 11:18:17 +0900

    [rlaehdrbs90] 코드리뷰 수정사항 반영, login db와 연결완료

--- services/auth_service.py
+++ services/auth_service.py
@@ -3,7 +3,7 @@ from datetime import datetime,timezone
 
 from core.db_connection import get_db_connection
 from models.user import UserInternal
-from repositories.user_repo import get_users, save_users, get_users_db
+from repositories.user_repo import get_users
 from services.jwt_service import create_access_token
 from models.auth import UserSignUp, UserLogin
 from fastapi import HTTPException
@@ -18,17 +18,18 @@ def verify_password(plain_password : str, hashed_password :str):
     return pwd_context.verify(plain_password, hashed_password)
 
 def signup_user(data: UserSignUp):
-    con = get_db_connection()
+    con = None
     try:
+        con = get_db_connection()
         with con.cursor() as cursor:
-            check_sql = "SELECT * FROM users WHERE email = %s"
+            check_sql = "SELECT email FROM users WHERE email = %s"
             cursor.execute(check_sql, (data.email, ))
             if cursor.fetchone():
                 raise HTTPException(status_code=400, detail="이미 존재하는 이메일")
 
             hashed_password = get_password_hash(data.password)
             user_id = str(uuid.uuid4())
-            user_creat_at = datetime.now(timezone.utc)
+            user_created_at  = datetime.now(timezone.utc)
 
             insert_sql = "INSERT INTO users (user_id, email, password, nickname, profile_image_url, created_at) VALUES (%s, %s, %s, %s, %s, %s)"
 
@@ -38,7 +39,7 @@ def signup_user(data: UserSignUp):
                 hashed_password,
                 data.nickname,
                 data.profile_image_url,
-                user_creat_at
+                user_created_at
             ))
 
             user=UserInternal(
@@ -47,24 +48,36 @@ def signup_user(data: UserSignUp):
                 nickname=data.nickname,
                 password=hashed_password,
                 profile_image_url=data.profile_image_url,
-                created_at=user_creat_at
+                created_at=user_created_at
             )
         con.commit()
         token, expires = create_access_token(user.user_id)
         return user, token, expires
+    except Exception :
+        con.rollback()
+        raise
+    finally:
+        if con :
+            con.close()
+
+def login_user(data: UserLogin):
+    con = get_db_connection()
+    try:
+        with con.cursor() as cursor:
+            check_sql = "SELECT user_id,email, password, nickname, profile_image_url FROM users where email = %s"
+            cursor.execute(check_sql, (data.username,))
+            user_record = cursor.fetchone()
+            print(user_record)
+            if user_record is None:
+                raise HTTPException(status_code=401, detail="유저 정보(이메일 혹은 비밀번호)가 없습니다.")
+
+            if not verify_password(data.password, user_record["password"]):
+                raise HTTPException(401, "인증 실패")
+
+            token, expires = create_access_token(user_record["user_id"])
+            return user_record, token, expires
     except Exception as ex:
         con.rollback()
         raise ex
     finally:
-        if cursor:
-            cursor.close()
         con.close()
-
-def login_user(data: UserLogin):
-    users = get_users()
-    user = next((u for u in users if u["email"] == data.username), None)
-
-    if not user or not verify_password(data.password, user["password"]):
-        raise HTTPException(401, "인증 실패")
-
-    return user, *create_access_token(user["user_id"])

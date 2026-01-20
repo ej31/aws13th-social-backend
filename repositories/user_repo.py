0d4e4204ec7e354ffa8ad3bcf8cf2d170f80df85
eewwqq1 <rlaehdrbs90@gmail.com>
2026-01-20 22:43:05 +0900

    [rlaehdrbs90] 코드리뷰 수정사항 반영

--- repositories/user_repo.py
+++ repositories/user_repo.py
@@ -25,17 +25,3 @@ def get_user_by_id(user_id):
    finally:
        if conn:
             conn.close()
-
-
-# def get_users():
-#     if not os.path.exists(DB_FILE):
-#         return []
-#     with open(DB_FILE, "r", encoding="utf-8") as f:
-#         try:
-#             return json.load(f)
-#         except json.JSONDecodeError:
-#             return []
-
-# def save_users(users: list[dict]):
-#     with open(DB_FILE, "w", encoding="utf-8") as f:
-#         json.dump(users, f, indent=4, ensure_ascii=False)

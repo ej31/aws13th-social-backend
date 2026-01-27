d477c12b897d38121b10ca08b0a747278d76dcd5
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2026-01-27 18:34:15 +0900

    refactor: AsyncSessionLocal() 빠진거 추가

--- db/session.py
+++ db/session.py
@@ -20,7 +20,7 @@ AsyncSessionLocal = async_sessionmaker(
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    async with AsyncSessionLocal as session:
+    async with AsyncSessionLocal() as session:
         try:
             yield session
             await session.commit()
